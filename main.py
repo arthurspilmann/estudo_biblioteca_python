1c1e8090a7a79fd2128674128dd4190fd0d5802c
arthur <arthur.spilmann@gmail.com>
2026-02-02 14:33:32 -0300

    corrigindo bug no put

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional 
 
 app = FastAPI()
 
 meus_livros = {}
+
+class Livro(BaseModel):
+    titulo: str
+    autor: str
+    ano_publicacao: int
 
 @app.get("/")
 def hello_world():
@@ -21,29 +28,21 @@ def get_livros():
 # ano de publicação
 
 @app.post("/adiciona")
-def post_livros(id_livro: int, titulo: str, autor: str, ano_publicacao: int):
+def post_livros(id_livro: int, livro: Livro):
     if id_livro in meus_livros:
         raise HTTPException(status_code=400, detail="Livro com este ID já existe.")
     else:
-        meus_livros[id_livro] = {
-            "titulo": titulo,
-            "autor": autor,
-            "ano_publicacao": ano_publicacao
-        }
+        meus_livros[id_livro] = livro.model_dump() #livro.dict() foi atualizado para livro.model_dump() na versão mais recente do Pydantic
         return {"message": "Livro adicionado com sucesso.", "livro": meus_livros[id_livro]}
     
 @app.put("/atualiza/{id_livro}")
-def put_livros(id_livro: int, titulo: str = None, autor: str = None, ano_publicacao: int = None):
+def put_livros(id_livro: int, livro: Livro):
+    meu_livro = meus_livros.get(id_livro)
     if id_livro not in meus_livros:
         raise HTTPException(status_code=404, detail="Livro não encontrado.")
     else:
-        if titulo:
-            meus_livros[id_livro]["titulo"] = titulo
-        if autor:
-            meus_livros[id_livro]["autor"] = autor
-        if ano_publicacao:
-            meus_livros[id_livro]["ano_publicacao"] = ano_publicacao
-        return {"message": "Livro atualizado com sucesso.", "livro": meus_livros[id_livro]}
+        meus_livros[id_livro] = livro.model_dump() #livro.dict() foi atualizado para livro.model_dump() na versão mais recente do Pydantic
+        return {"message": "Livro atualizado com sucesso."}    
     
 @app.delete("/deleta/{id_livro}")
 def delete_livros(id_livro: int):
